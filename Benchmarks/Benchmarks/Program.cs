f4d344630f0b02fbfab383d54a9a0f7ba6c396b6
Aallix <126614969+Aallix@users.noreply.github.com>
2023-03-01 15:21:05 +0300

    Add string benchmarking for List and LinkedList

--- Benchmarks/Benchmarks/Program.cs
+++ Benchmarks/Benchmarks/Program.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 BenchmarkRunner.Run<ListBenchmarkInteger>();
+BenchmarkRunner.Run<ListBenchmarkString>();
 
 [MemoryDiagnoser]
 public class ListBenchmarkInteger
@@ -11,7 +12,7 @@ public class ListBenchmarkInteger
     public int size;
 
     [Benchmark(Baseline = true)]
-    public void ListFill()
+    public void ListFillInt()
     {
         var list = new List<int>();
         for (int i = 0; i < size; i++)
@@ -21,12 +22,39 @@ public class ListBenchmarkInteger
     }
 
     [Benchmark]
-    public void LinkedListFill()
+    public void LinkedListFillInt()
     {
         var linkedList = new LinkedList<int>();
         for (int i = 0; i < size; i++)
         {
             linkedList.AddLast(i);
+        }
+    }
+}
+
+[MemoryDiagnoser]
+public class ListBenchmarkString
+{
+    [Params(1000, 10000)]
+    public int size;
+
+    [Benchmark(Baseline = true)]
+    public void ListFillString()
+    {
+        var list = new List<string>();
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i.ToString());
+        }
+    }
+
+    [Benchmark]
+    public void LinkedListFillString()
+    {
+        var linkedList = new LinkedList<string>();
+        for (int i = 0; i < size; i++)
+        {
+            linkedList.AddLast(i.ToString());
         }
     }
 }
