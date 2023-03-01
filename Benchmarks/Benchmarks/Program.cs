f7be08b5bdab80933f7e062316a53a86a8027489
Aallix <126614969+Aallix@users.noreply.github.com>
2023-03-01 14:59:44 +0300

    Add integer benchmarking for List and Linked List

--- Benchmarks/Benchmarks/Program.cs
+++ Benchmarks/Benchmarks/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ListBenchmarkInteger>();
+
+[MemoryDiagnoser]
+public class ListBenchmarkInteger
+{
+    [Params(1000, 10000)]
+    public int size;
+
+    [Benchmark(Baseline = true)]
+    public void ListFill()
+    {
+        var list = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+        }
+    }
+
+    [Benchmark]
+    public void LinkedListFill()
+    {
+        var linkedList = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            linkedList.AddLast(i);
+        }
+    }
+}
